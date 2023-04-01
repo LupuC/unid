dbeef954954ed9adbc342e43354c9127dbe0898f
Lupu Claudiu-Andrei <lupuclaudiuandrei@gmail.com>
2023-04-02 00:48:01 +0300

    Added UUID5 generator + validator

--- uniid.ts
+++ uniid.ts
@@ -1,21 +1,6 @@
+import * as crypto from 'crypto';
+
 export const unid = {
-  generate1(): string {
-    const now = new Date().getTime();
-    const epoch = new Date(1582, 9, 15).getTime();
-    const ticks = (now - epoch) * 10000;
-    const nodeId = 92759;
-    const clockSeq = Math.floor(Math.random() * 16384); // random 14-bit sequence number
-    const unid = [
-      (ticks & 0xffffffff).toString(16).padStart(8, '0'),
-      ((ticks >> 32) & 0xffff).toString(16).padStart(4, '0'),
-      ((ticks >> 48) & 0x0fff | 0x1000).toString(16).padStart(4, '0'),
-      (nodeId & 0xffff).toString(16).padStart(4, '0'),
-      ((nodeId >> 16) & 0xff | 0x80).toString(16).padStart(2, '0'),
-      (clockSeq & 0x3fff | 0x8000).toString(16).padStart(4, '0')
-    ].join('-');
-    return unid;
-  },
-  
   generate4(): string {
     let d = new Date().getTime();
     if (isNaN(d)) {
@@ -29,13 +14,42 @@ export const unid = {
     return unid;
   },
 
+  // check(uuidString: string): boolean {
+  //   try {
+  //     const uuidPattern = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
+  //     return uuidPattern.test(uuidString);
+  //   } catch (err) {
+  //     console.error(`Error validating UUID: ${err}`);
+  //     return false;
+  //   }
+  // },
+
+  generateUUID5(namespace: string, name: string): string {
+    const nsBuffer = Buffer.from(namespace.replace(/-/g, ''), 'hex');
+    const nameBuffer = Buffer.from(name, 'utf8');
+    const hash = crypto.createHash('sha1');
+    hash.update(nsBuffer);
+    hash.update(nameBuffer);
+    const bytes = hash.digest();
+  
+    bytes[6] = (bytes[6] & 0x0f) | 0x50;
+    bytes[8] = (bytes[8] & 0x3f) | 0x80;
+  
+    const hex = bytes.toString('hex');
+    const uuid5 = `${hex.substr(0, 8)}-${hex.substr(8, 4)}-5${hex.substr(13, 3)}-${hex.substr(16, 4)}-${hex.substr(20, 12)}`;
+    return uuid5;
+  },
+  
+
   check(uuidString: string): boolean {
     try {
-      const uuidPattern = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
-      return uuidPattern.test(uuidString);
+      const uuid4Pattern = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
+      const uuid5Pattern = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-5[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
+      return uuid4Pattern.test(uuidString) || uuid5Pattern.test(uuidString);
     } catch (err) {
       console.error(`Error validating UUID: ${err}`);
       return false;
     }
-  }
+  },
+  
 };
